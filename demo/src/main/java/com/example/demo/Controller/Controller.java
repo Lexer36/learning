20b028b0a1b49c4e89ba9014d05a55042c014f3b
Lexer36 <108135950+Lexer36@users.noreply.github.com>
2022-10-11 15:45:12 +0300

    Update Controller.java

--- demo/src/main/java/com/example/demo/Controller/Controller.java
+++ demo/src/main/java/com/example/demo/Controller/Controller.java
@@ -1,10 +1,7 @@
 package com.example.demo.Controller;
 
-import com.example.demo.Service.SalaryService;
 import com.example.demo.model.SalaryDTO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
